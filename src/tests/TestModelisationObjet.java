83b0fe061f8d6b695ea5094ed4837e58d9b1f679
Mickael <mickael.spilmont.fr@gmail.com>
2018-04-11 17:22:19 +0200

    Ajout TestModelisationObjet.java

---
+++ src/tests/TestModelisationObjet.java
@@ -0,0 +1,18 @@
+package tests;
+
+import tpModelisationObjet.Element;
+import tpModelisationObjet.Piece;
+
+public class TestModelisationObjet {
+	
+	public static void main(String[] args) {
+		Piece piece01 = new Piece("Nvidia GTX 1080", 1536);
+		Piece piece02 = new Piece("8 GB GDDR5", 1552);
+		
+		Element element01 = new Element("Carte graphique");
+		
+		System.out.println(element01);
+		
+		System.out.println("Exit 0");
+	}
+}
