2b4a29c979ff833e53001c9431eb69a72a423fda
Mickael <mickael.spilmont.fr@gmail.com>
2018-04-11 16:50:41 +0200

    Ajout de la classe abstraite Assemblage.java et de la classe Element.java.

---
+++ src/tpModelisationObjet/Assemblage.java
@@ -0,0 +1,31 @@
+package tpModelisationObjet;
+
+import java.util.ArrayList;
+
+public abstract class Assemblage {
+	private String nom;
+	private ArrayList<Assemblable> assemblables;
+	
+	public Assemblage(String nom) {
+		this.nom = nom;
+		assemblables = new ArrayList<>();
+	}
+	
+	public boolean add(Assemblable assemblable) {
+		return assemblables.add(assemblable);
+	}
+	
+	public boolean remove(Assemblable assemblable) {
+		return assemblables.remove(assemblable);
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public String toString() {
+		StringBuilder chaine = new StringBuilder();
+		chaine.append(nom).append(" :\n");
+		return chaine.toString();
+	}
+}
---
+++ src/tpModelisationObjet/Element.java
@@ -0,0 +1,8 @@
+package tpModelisationObjet;
+
+public class Element extends Assemblage implements Assemblable {
+	
+	public Element(String nom) {
+		super(nom);
+	}
+}
