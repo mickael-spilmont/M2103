e60d609ac1ae6e942ae1dc5afea2f37b566a797b
mickael <mickael.spilmont.fr@gmail.com>
2018-04-15 15:36:59 +0200

    Finalisation des tests de la classe Salles.

--- src/tests/TestDS2.java
+++ src/tests/TestDS2.java
@@ -1,30 +1,40 @@
 package tests;
 
 import entrainementDS2.Salle;
+import entrainementDS2.Salles;
 import entrainementDS2.Seance;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestDS2 {
     public static int HORAIRE_MAX = 10;
 
     public static void main(String[] args) {
+        List<Seance> seances = new ArrayList<>();
+
+        Salles salles = new Salles();
+
         Salle salle1 = new Salle("0A15", "Cour", 25);
         Salle salle2 = new Salle("4A28", "TP", 15);
 
-        Seance seance1 = new Seance("POO", "TP", 4, 15);
-        Seance seance2 = new Seance("Gestion", "Cour", 2, 20);
-        Seance seance3 = new Seance("Reseau", "TP", 2, 15);
-        Seance seance4 = new Seance("Math", "Cour", 4, 20);
-        Seance seance5 = new Seance("Droit", "Cour", 2, 15);
-        Seance seance6 = new Seance("Systeme", "TP", 4, 15);
+        salles.ajouterSalle(salle1);
+        salles.ajouterSalle(salle2);
 
-        salle2.accueillir(seance1);
-        salle1.accueillir(seance2);
-        salle2.accueillir(seance3);
-        salle1.accueillir(seance4);
-        salle1.accueillir(seance5);
-        salle2.accueillir(seance6);
 
-        System.out.println(salle1);
-        System.out.println(salle2);
+        seances.add(new Seance("POO", "TP", 4, 15));
+        seances.add(new Seance("Gestion", "Cour", 2, 20));
+        seances.add(new Seance("Reseau", "TP", 2, 15));
+        seances.add(new Seance("Math", "Cour", 4, 20));
+        seances.add(new Seance("Droit", "Cour", 2, 15));
+        seances.add(new Seance("Systeme", "TP", 4, 15));
+        seances.add(new Seance("COO", "TP", 4, 15));
+        seances.add(new Seance("Algo", "TP", 4, 15));
+        seances.add(new Seance("Projet", "TP", 4, 15));
+        seances.add(new Seance("Anglais", "Cour", 2, 15));
+
+        System.out.println(salles.planifier(seances));
+
+        System.out.println(salles);
     }
 }
